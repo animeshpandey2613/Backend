0912350345dbd699348d541f2d68b371fe8058b4
animeshpandey2613 <121745585+animeshpandey2613@users.noreply.github.com>
2024-05-13 13:16:25 +0530

    1

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use("/users", UserRouter);
 app.use("/movies", MovieRouter);
-app.use('/.netlify/functions/api/users', UserRouter);
-app.use('/.netlify/functions/api/movies', MovieRouter); 
+app.use('/', UserRouter);
+app.use('/', MovieRouter); 
 
 app.all("*", (req, res, next) => next(new AppError("Invalid Route", 404)));
 app.use(ErrorController);
