8816e655d6a701f44d05e53385b574a7d8677b8b
animeshpandey2613 <121745585+animeshpandey2613@users.noreply.github.com>
2024-05-13 13:01:27 +0530

    1

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const serverless = require('serverless-http');
 process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   console.log("Uncaught Exception, Shutting down the server");
   process.exit(1);
 });
+dotenv.config({ path: "./config.env" });
 const app = require("../app");
 
 const DB = process.env.DATABASE.replace(
@@ -11,11 +14,14 @@ const DB = process.env.DATABASE.replace(
   process.env.PASSWORD
 ).replace("<USER>", process.env.USER);
 mongoose.connect(DB).then(() => console.log("Database Connected"));
+const server = serverless(app);
 
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled rejection, Shutting down the server");
+  server.close(() => {
     process.exit(1);
+  });
 });
 
-module.exports.handler = app;
+module.exports.handler = server;
